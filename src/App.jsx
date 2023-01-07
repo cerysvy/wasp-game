c57251edbbd0002016f2b691d1e9a57678493a4b
Cerys <cerysvyner96@gmail.com>
2023-01-07 19:52:48 +0000

    improved game functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,94 +1,110 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
+
+let queen = { 
+  name: 'Queen', 
+  health: 1,
+  alive: true
+};
+
+let drones = new Array(8).fill(null).map(() => ({"name":"Drone", "health":60, "alive":true}));
+let workers = new Array(5).fill(null).map(() => ({"name":"Worker", "health":68, "alive":true}));
+let waspNest = [...drones, queen, ...workers];
 
 const App = () => {
 
-  //const [isPlaying, setIsPlaying] = useState(false);
-  //const [isGameOver, setIsGameOver] = useState(false);
+  // const [isPlaying, setIsPlaying] = useState(false);
+  // const [isGameOver, setIsGameOver] = useState(false);
 
   //START SCREEN: {!isPlaying} startButton OnClick={() = setIsPlaying(true)} && 
   //PLAY SCREEN: {isPlaying} health = 0 leads to setIsGameOver(true)
   //GAME OVER SCREEN: {!isPlaying} newGameButton OnClick={() = setIsPlaying(true)}
 
-  let drone = { 
-    name: 'Drone', 
-    health: 60,
-    alive: true
-  };
+    // const [droneHealth, setDroneHealth] = useState(60);
+    // const [queenHealth, setQueenHealth] = useState(80);
+    // const [workerHealth, setWorkerHealth] = useState(68);
+    // const [isDroneAlive, setIsDroneAlive] = useState(true);
 
-  let queen = { 
-    name: 'Queen', 
-    health: 80,
-    alive: true
-  };
+  console.log(waspNest);
 
-  let worker = { 
-    name: 'Worker', 
-    health: 68,
-    alive: true
-  };
+  const [waspsNestArr, setWaspsNestArr] = useState(waspNest);
 
-  let waspsNest = [
-    ...Array(8).fill(drone),
-    queen,
-    ...Array(5).fill(worker)
-  ];
-
-  // start game 
-
-  // do while - loop
-  // shoot() method, listens to shoot button
-  // choose randm wasp from waspsNest
-  // run score method on that wasp
-  // then count all alive the wasps in the array, if none then game over
-
-  function gameTurn() {
-    let wasp = chooseWasp();
-    shoot(wasp);
-  }
-
-  function chooseWasp() {
-    let aliveWasps = waspsNest.filter(wasp => wasp.alive);
+  function chooseWasp(wasps) {
+    let aliveWasps = wasps.filter(wasp => wasp.alive);
     let wasp = aliveWasps[Math.floor(Math.random()*aliveWasps.length)];
     return wasp;
   }
 
   function shoot(wasp) {
+    console.log("inside shoot");
+    console.log(wasp);
     if(wasp.name === "Drone") {
-      wasp.health=(wasp.health-12)
+      (wasp.health = wasp.health-12)
     }
     if(wasp.name === "Queen") {
-      wasp.health=(wasp.health-7)
+      wasp.health = wasp.health-7
+      //setQueenHealth(queenHealth-7)
     }
     if(wasp.name === "Worker") {
-      wasp.health=(wasp.health-10)
+      wasp.health = wasp.health-10
+      //setWorkerHealth(workerHealth-10)
     }
     if(wasp.health <= 0) {
       wasp.alive=false
     }
+    console.log("after shoot");
+    console.log(wasp);
   }
   
-  //check the queen is alive
-  //check all the wasps are alive at the end of each click
+  function gameTurn() {
+    let wasps = [...waspsNestArr];
+    console.log(wasps)
+    let wasp = chooseWasp(wasps);
+    console.log(wasp);
+    shoot(wasp);
+    console.log(wasps);
+    let newWasps = checkQueen(wasps);
+    setWaspsNestArr(newWasps);
+  }
 
-  let DisplayWasps = () => {
-    return waspsNest.map((wasp) => (
-      <p>{wasp.name} - {wasp.health}</p>
+  function checkQueen(wasps) {
+    let queen = wasps.filter(wasp => wasp.name == "Queen");
+    console.log(queen);
+    if (!queen.alive) {
+      for (let i = 0; i < wasps.length; i++) {
+        wasps[i].health = 0;
+        wasps[i].alive = false;
+      }};
+    console.log(wasps)
+    return wasps;
+  };
+
+  const DisplayWasps = () => {
+    return waspsNestArr.filter(wasp => (wasp.alive === true)).map((wasp, index) => (
+      <p key ={index}>{wasp.name} - {wasp.health}</p>
     ))
   };
-  
 
   // if isPlaying == false show start screen/game over screen
   // if isPlaying == true show game
 
   return (
-    <div>
-      <p>
+    <>
+    {/* {!isPlaying && !isGameOver && 
+      <div>
+        Start Menu
+      </div>
+    }
+    {isPlaying && !isGameOver && */}
+      <div>
         <DisplayWasps />
-      </p>
-      <button onClick={gameTurn}>
-        Shoot
-      </button>
-    </div>
+        <button onClick={gameTurn}>
+          Shoot
+        </button>
+      </div>
+    {/*}}
+    {!isPlaying && isGameOver && <div>Game Over</div>}*/}
+    </>
+
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+//testing core engine of the game...
+
+//test shoot method reduces score on a wasp
+//check 0 health => dead?
+//check queen dies all die
+//check all die game over
