87497baa92e5dea72f825521e57d678de659d380
Cerys <cerysvyner96@gmail.com>
2023-01-07 22:43:48 +0000

    decreasing array but queen kill not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,33 @@
 import React, {useEffect, useState} from "react";
 
-let queen = { 
-  name: 'Queen', 
-  health: 1,
-  alive: true
-};
+// let queen = { 
+//   name: 'Queen', 
+//   health: 1,
+//   alive: true
+// };
 
-let drones = new Array(8).fill(null).map(() => ({"name":"Drone", "health":60, "alive":true}));
-let workers = new Array(5).fill(null).map(() => ({"name":"Worker", "health":68, "alive":true}));
-let waspNest = [...drones, queen, ...workers];
+// let drones = new Array(8).fill(null).map(() => ({"name":"Drone", "health":60, "alive":true}));
+// let workers = new Array(5).fill(null).map(() => ({"name":"Worker", "health":68, "alive":true}));
+// let waspNest = [...drones, queen, ...workers];
 
 const App = () => {
+
+  let waspNest = [
+    {name: 'Drone', health: 60, alive: true, index: 0},
+    {name: 'Drone', health: 60, alive: true, index: 1},
+    {name: 'Drone', health: 60, alive: true, index: 2},
+    {name: 'Drone', health: 60, alive: true, index: 3},
+    {name: 'Drone', health: 60, alive: true, index: 4},
+    {name: 'Drone', health: 60, alive: true, index: 5},
+    {name: 'Drone', health: 60, alive: true, index: 6},
+    {name: 'Drone', health: 60, alive: true, index: 7},
+    {name: 'Queen', health: 80, alive: true, index: 8},
+    {name: 'Worker', health: 68, alive: true, index: 9},
+    {name: 'Worker', health: 68, alive: true, index: 10},
+    {name: 'Worker', health: 68, alive: true, index: 11},
+    {name: 'Worker', health: 68, alive: true, index: 12},
+    {name: 'Worker', health: 68, alive: true, index: 13}
+  ]
 
   // const [isPlaying, setIsPlaying] = useState(false);
   // const [isGameOver, setIsGameOver] = useState(false);
@@ -24,57 +41,47 @@ const App = () => {
     // const [workerHealth, setWorkerHealth] = useState(68);
     // const [isDroneAlive, setIsDroneAlive] = useState(true);
 
-  console.log(waspNest);
-
   const [waspsNestArr, setWaspsNestArr] = useState(waspNest);
 
   function chooseWasp(wasps) {
-    let aliveWasps = wasps.filter(wasp => wasp.alive);
-    let wasp = aliveWasps[Math.floor(Math.random()*aliveWasps.length)];
+    const aliveWasps = wasps.filter((wasp) => wasp.alive);
+    console.log(aliveWasps);
+    const wasp = aliveWasps[Math.floor(Math.random()*aliveWasps.length)];
     return wasp;
   }
 
   function shoot(wasp) {
-    console.log("inside shoot");
-    console.log(wasp);
     if(wasp.name === "Drone") {
-      (wasp.health = wasp.health-12)
+      wasp.health = wasp.health-12
     }
     if(wasp.name === "Queen") {
       wasp.health = wasp.health-7
-      //setQueenHealth(queenHealth-7)
     }
     if(wasp.name === "Worker") {
       wasp.health = wasp.health-10
-      //setWorkerHealth(workerHealth-10)
     }
     if(wasp.health <= 0) {
       wasp.alive=false
     }
-    console.log("after shoot");
-    console.log(wasp);
   }
+  
   
   function gameTurn() {
     let wasps = [...waspsNestArr];
-    console.log(wasps)
     let wasp = chooseWasp(wasps);
     console.log(wasp);
     shoot(wasp);
-    console.log(wasps);
     let newWasps = checkQueen(wasps);
     setWaspsNestArr(newWasps);
   }
 
   function checkQueen(wasps) {
-    let queen = wasps.filter(wasp => wasp.name == "Queen");
-    console.log(queen);
-    if (!queen.alive) {
-      for (let i = 0; i < wasps.length; i++) {
+    let queen = wasps.filter(wasp => wasp.name=="Queen");
+    if (queen.alive==false) {
+      for (let i=0; i < wasps.length; i++) {
         wasps[i].health = 0;
         wasps[i].alive = false;
       }};
-    console.log(wasps)
     return wasps;
   };
 
