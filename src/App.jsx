19246ee106f44812167f454c60e6ef8b38b03c6d
Cerys <cerysvyner96@gmail.com>
2023-01-08 11:43:47 +0000

    more game functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,33 @@
-import React, {useEffect, useState} from "react";
+import React, { useState } from "react";
 
-// let queen = { 
-//   name: 'Queen', 
-//   health: 1,
-//   alive: true
-// };
+let queen = { 
+  name: 'Queen', 
+  health: 1,
+  alive: true
+};
 
-// let drones = new Array(8).fill(null).map(() => ({"name":"Drone", "health":60, "alive":true}));
-// let workers = new Array(5).fill(null).map(() => ({"name":"Worker", "health":68, "alive":true}));
-// let waspNest = [...drones, queen, ...workers];
+let drones = new Array(8).fill(null).map(() => ({"name":"Drone", "health":60, "alive":true}));
+let workers = new Array(5).fill(null).map(() => ({"name":"Worker", "health":68, "alive":true}));
+let waspNest = [...drones, queen, ...workers];
 
 const App = () => {
 
-  let waspNest = [
-    {name: 'Drone', health: 60, alive: true, index: 0},
-    {name: 'Drone', health: 60, alive: true, index: 1},
-    {name: 'Drone', health: 60, alive: true, index: 2},
-    {name: 'Drone', health: 60, alive: true, index: 3},
-    {name: 'Drone', health: 60, alive: true, index: 4},
-    {name: 'Drone', health: 60, alive: true, index: 5},
-    {name: 'Drone', health: 60, alive: true, index: 6},
-    {name: 'Drone', health: 60, alive: true, index: 7},
-    {name: 'Queen', health: 80, alive: true, index: 8},
-    {name: 'Worker', health: 68, alive: true, index: 9},
-    {name: 'Worker', health: 68, alive: true, index: 10},
-    {name: 'Worker', health: 68, alive: true, index: 11},
-    {name: 'Worker', health: 68, alive: true, index: 12},
-    {name: 'Worker', health: 68, alive: true, index: 13}
-  ]
+  // let waspNest = [
+  //   {name: 'Drone', health: 60, alive: true, index: 0},
+  //   {name: 'Drone', health: 60, alive: true, index: 1},
+  //   {name: 'Drone', health: 60, alive: true, index: 2},
+  //   {name: 'Drone', health: 60, alive: true, index: 3},
+  //   {name: 'Drone', health: 60, alive: true, index: 4},
+  //   {name: 'Drone', health: 60, alive: true, index: 5},
+  //   {name: 'Drone', health: 60, alive: true, index: 6},
+  //   {name: 'Drone', health: 60, alive: true, index: 7},
+  //   {name: 'Queen', health: 1, alive: true, index: 8},
+  //   {name: 'Worker', health: 68, alive: true, index: 9},
+  //   {name: 'Worker', health: 68, alive: true, index: 10},
+  //   {name: 'Worker', health: 68, alive: true, index: 11},
+  //   {name: 'Worker', health: 68, alive: true, index: 12},
+  //   {name: 'Worker', health: 68, alive: true, index: 13}
+  // ]
 
   // const [isPlaying, setIsPlaying] = useState(false);
   // const [isGameOver, setIsGameOver] = useState(false);
@@ -35,11 +35,6 @@ const App = () => {
   //START SCREEN: {!isPlaying} startButton OnClick={() = setIsPlaying(true)} && 
   //PLAY SCREEN: {isPlaying} health = 0 leads to setIsGameOver(true)
   //GAME OVER SCREEN: {!isPlaying} newGameButton OnClick={() = setIsPlaying(true)}
-
-    // const [droneHealth, setDroneHealth] = useState(60);
-    // const [queenHealth, setQueenHealth] = useState(80);
-    // const [workerHealth, setWorkerHealth] = useState(68);
-    // const [isDroneAlive, setIsDroneAlive] = useState(true);
 
   const [waspsNestArr, setWaspsNestArr] = useState(waspNest);
 
@@ -65,34 +60,51 @@ const App = () => {
     }
   }
   
-  
   function gameTurn() {
     let wasps = [...waspsNestArr];
-    let wasp = chooseWasp(wasps);
-    console.log(wasp);
+    const wasp = chooseWasp(wasps);
     shoot(wasp);
-    let newWasps = checkQueen(wasps);
+    const newWasps = checkQueen(wasps);
     setWaspsNestArr(newWasps);
   }
 
   function checkQueen(wasps) {
-    let queen = wasps.filter(wasp => wasp.name=="Queen");
-    if (queen.alive==false) {
-      for (let i=0; i < wasps.length; i++) {
-        wasps[i].health = 0;
-        wasps[i].alive = false;
-      }};
+    let queen = wasps.filter(wasp => wasp.name==="Queen");
+    // if(queen.alive===false) {
+    //   for (let i=0; i<wasps.length; i++) {
+    //     wasps[i].health = 0;
+    //     wasps[i].alive = false;
+    //   }};
+    console.log(queen);
+    if(queen.alive===false) {
+      wasps = killAllWasps(wasps);
+    };
     return wasps;
   };
+
+  function killAllWasps(wasps) {
+    wasps = wasps.forEach((wasp) => ({
+      ...wasp, health:0, alive:false
+    }))
+  };
+
+  // function killAllWasps(wasps, aliveWasps) {
+  //   console.log(wasps);
+  //   for (let i=0; i<wasps.length; i++) {
+  //     wasps[i].health = 0;
+  //     wasps[i].alive = false;
+  //   }
+  //   console.log(aliveWasps);
+  // }
+
+  //fill array on isPlaying true??
+  // if array empty !isPlaying
 
   const DisplayWasps = () => {
     return waspsNestArr.filter(wasp => (wasp.alive === true)).map((wasp, index) => (
       <p key ={index}>{wasp.name} - {wasp.health}</p>
     ))
   };
-
-  // if isPlaying == false show start screen/game over screen
-  // if isPlaying == true show game
 
   return (
     <>
@@ -103,10 +115,10 @@ const App = () => {
     }
     {isPlaying && !isGameOver && */}
       <div>
-        <DisplayWasps />
+        
         <button onClick={gameTurn}>
           Shoot
-        </button>
+        </button><DisplayWasps />
       </div>
     {/*}}
     {!isPlaying && isGameOver && <div>Game Over</div>}*/}
