afe7c0cbd7449caeb40ce5e48903603fae3ea042
Cerys <cerysvyner96@gmail.com>
2023-01-09 09:17:38 +0000

    Full stylings

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import styles from "./index.css";
 
 const App = () => {
 
@@ -15,17 +14,35 @@ const App = () => {
     const queen = {
       name: "Queen",
       img: "./queen.png",
+      alt: "https://www.flaticon.com/free-icon/bee_4794502",
       health: 80,
       alive: true,
-      className: "queenStyles"
+      className: "queen",
+      height: "100px"
     };
     const drones = new Array(8)
       .fill(null)
-      .map(() => ({ name: "Drone", img: "./drone.png", health: 60, alive: true, className: "droneStyles" }));
+      .map(() => ({ 
+        name: "Drone", 
+        img: "./drone.png", 
+        alt: "https://www.flaticon.com/free-icon/wasp_311590", 
+        health: 60,
+        alive: true,
+        className: "drone",
+        height: "70px"
+      }));
     const workers = new Array(5)
       .fill(null)
-      .map(() => ({ name: "Worker", img: "./worker.png", health: 68, alive: true, className: "workerStyles" }));
-    const waspNest = [...drones, queen, ...workers];
+      .map(() => ({ 
+        name: "Worker", 
+        img: "./worker.png",
+        alt: "https://www.flaticon.com/free-icon/wasp_1850229", 
+        health: 68, 
+        alive: true, 
+        className: "worker", 
+        height: "70px" 
+      }));
+    const waspNest = [queen, ...drones, ...workers];
     setWaspsNestArr(waspNest);
   };
 
@@ -38,12 +55,16 @@ const App = () => {
   function shoot(wasp) {
     if(wasp.name === "Drone") {
       wasp.health = wasp.health-12
+
     }
     if(wasp.name === "Queen") {
       wasp.health = wasp.health-7
+
     }
     if(wasp.name === "Worker") {
-      wasp.health = wasp.health-10
+      wasp.health = wasp.health-10;
+      // className = workerHit;
+      // className = workerUnhit;
     }
     if(wasp.health <= 0) {
       wasp.alive=false
@@ -76,24 +97,6 @@ const App = () => {
   };
 
 const DisplayWasps = () => {
-  // let aliveWasps = waspsNestArr.filter((wasp) => wasp.alive);
-  // if (aliveWasps.length > 0) {
-  //   return(aliveWasps.map((wasp, index) => (
-  //     <>
-  //     {alive.wasps.map((wasp, index) => (
-  //       <div>
-  //         <p key={index}>
-  //           {wasp.health}
-  //         </p>
-  //         <img src={`src/img/${wasp.name}.png`}/>
-  //       </div>
-  //     ))}
-  //     <button onClick={gameTurn}>
-  //       Shoot
-  //     </button>
-  //     </>
-  //   )))
-  // }
   if (waspsNestArr.filter((wasp) => wasp.alive).length > 0) {
     let aliveWasps = waspsNestArr.filter((wasp) => wasp.alive)
     if (aliveWasps.length > 0) {
@@ -101,26 +104,33 @@ const DisplayWasps = () => {
       <>
         {aliveWasps
           .map((wasp, index) => (
-            <div  className={styles.backgroundContainer}>
-              <div className={wasp.className}>
-                <p key={index}>
-                  {wasp.health}
-                </p>
-              <img src={wasp.img} width="50" height="50" />
-              </div>
-
+            <div className={wasp.className}>
+              <p key={index} className="score">
+                {wasp.health}
+              </p>  
+              <img src={wasp.img} height={wasp.height} alt={wasp.alt}/>
             </div>
           ))}
-
-
-        <button onClick={gameTurn}>Shoot</button>
       </>
     )}
     } else {
     return (
       <>
-        <p>Game Over</p>
-        <button onClick={startGame}>New Game</button>
+        <button onClick={startGame} className="newGameDeadQueenButton">
+          <img 
+            src="./queen.png"
+            alt="https://www.flaticon.com/free-icon/bee_4794502"
+            className="deadQueen"
+          />
+        </button>
+        <p className="newGameText">
+        &lt; &lt; Click the queen to play again
+        </p>
+        <img
+          className="gameOverIcon"
+          src="./game-over.png" 
+          alt="https://www.flaticon.com/free-icon/game-over_7372017?term=game+over&page=1&position=73&origin=search&related_id=7372017"
+        />
       </>
     );
   }
@@ -129,13 +139,51 @@ const DisplayWasps = () => {
   if (isPlaying)
     return (
       <>
-        <div>
-          <DisplayWasps />
+        <div className="gameContainer">
+          <div className="gridContainer">
+            <div>
+              <img 
+                src="./nest.png" 
+                alt="https://www.flaticon.com/free-icon/beehive_2988574?related_id=2988574&origin=search"
+                className="nestIcon"
+              />
+            </div>
+            <DisplayWasps />
+          </div>
+          <button className="shootButton" onClick={gameTurn}>
+            <img 
+              src="./shoot.png" 
+              alt="https://www.flaticon.com/free-icon/scope_7030486?term=hit&page=1&position=43&origin=search&related_id=7030486" 
+              className="shootButtonIcon"
+            />
+          </button>
         </div>
       </>
     );
   if (!isPlaying) {
-    return <button onClick={startGame}>Start Game</button>;
+    return (
+      <>
+      <div className="startDiv">
+        <div className="startWording">
+          <h3>Wasp Game</h3>
+          <img className="startMenuLogo" src="./worker.png" alt="https://www.flaticon.com/free-icon/wasp_1850229" />
+          <p>
+            Press the button to shoot at a random wasp. Wasps have and lose different points depending on their type. If the queen dies, all wasps in the nest die with her.
+          </p>
+        </div>
+        <button onClick={startGame} className="startButton">
+          <img 
+            src="./start.png" 
+            alt="https://www.flaticon.com/free-icon/start_5243423?related_id=5241750&origin=search" 
+            className="startButtonIcon"
+          />
+        </button>
+        <p className="funFactText">
+          Fun fact: Drone wasps don't have a visible stinger...
+        </p>
+      </div>
+      </>
+    )
   }
   };
 
